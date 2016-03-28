7023e29abc9bc3164c645991375df03ebd17e224
Ryan Rosenlund <rrosenlund@live.com>
2016-03-28 12:26:38 -0600

    added new row of loops, set up for next stage of learning loops

--- js/main.js
+++ js/main.js
@@ -348,3 +348,14 @@ function playBall2(){
 	}
 }
 
+/*  INTRO TO LOOPS  */
+
+function howMany(selectObject) {
+  var numberSelected = 0;
+  for (var i = 0; i < selectObject.options.length; i++){
+    if (selectObject.options[i].selected) {
+      numberSelected++;
+    }
+  }
+  return numberSelected;
+}
